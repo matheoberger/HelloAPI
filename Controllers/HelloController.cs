e811ce061ac0ac961419d33cc05c8df143586d2c
Mathéo Berger <matheo.berger@viacesi.fr>
2021-03-04 09:15:30 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace microPOC2._4_docker.Controllers
+{
+    public class HelloController : ApiController
+    {
+        //Hello msg = new Hello ();
+        public HelloGenerator helloGenerator = new HelloGenerator();
+        public IHttpActionResult GetHello()
+        {
+            return Ok(helloGenerator.DoThisSweetSound());
+        }
+    }
+}
